db95ee37d89f5db94d726fb8f4ae36d93b5b90a7
Ben Shelbourne <benshelly@hotmail.co.uk>
2016-01-16 12:00:46 +0000

    initial commit

---
+++ rightBet/src/rightBet/winBetCalculator.py
@@ -0,0 +1,17 @@
+'''
+Created on 16 Jan 2016
+
+@author: BenShelly
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
